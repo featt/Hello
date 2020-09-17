d5693f849ba61a4209d8aafa0079b234a291fbd3
featt <70686748+featt@users.noreply.github.com>
2020-09-17 11:28:23 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,43 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double n1(double a, double b, double c)
+{
+	return ((b + sqrt(pow(b, 2) + 4 * a * c)) / 2 * a) - pow(a, 3) * c + pow(b, -2);
+}
+
+double n2(double a, double b, double c, double d)
+{
+	return ((a * b) / (c * d)) - ((a * b - c) / (c * d));
+}
+
+double n3(double x, double y)
+{
+	return ((sin(x) + cos(y)) / (cos(x) - sin(y))) * tan(x * y);
+}
+
+double n4(double x, double y)
+{
+	return ((x + y) / (y + 1)) - ((x * y - 12) / (34 + x));
+}
+
+int main()
+{
+	double a, b, c, d, x, y;
+	cout << "input a, b and c: ";
+	cin >> a >> b >> c;
+	cout << "Reuslt of case1 func = " << n1(a, b, c) << "\n";
+	cout << "input a, b, c and d: ";
+	cin >> a >> b >> c >> d;
+	cout << "Result of case2 func = " << n2(a, b, c, d) << "\n";
+	cout << "input x and y: ";
+	cin >> a >> b;
+	cout << "Result of case3 func = " << n3(a, b) << "\n";
+	cout << "input x and y: ";
+	cin >> a >> b;
+	cout << "Result of case4 func = " << n4(a, b) << "\n";
+	return 0;
+}
+
---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void perimeter_and_area(double x, double y)
+{
+	double z = sqrt(x * x + y * y);
+	double s = (x * y) / 2;
+	double p = x + y + z;
+	cout << "Perimeter = " << p << "\n";
+	cout << "Area = " << s << "\n";
+}
+
+void add_substract_multiplicate_divide(double x, double y)
+{
+	cout << "x+y=" << x + y << "\n";
+	cout << "x-y=" << x - y << "\n";
+	cout << "x/y=" << x / y << "\n";
+	cout << "x*y=" << x * y << "\n";
+}
+
+void circumference_and_area(double r)
+{
+	const double PI = 3.141592653589793238463;
+	cout << "Длина окружности = " << 2 * PI * r << "\n";
+	cout << "Площадь круга = " << PI * (r * r) << "\n";
+}
+
+void powers_of_pi()
+{
+	const double PI = 3.141592653589793238463;
+	for (int i = 2; i < 6; i++)
+	{
+		cout << "PI in " << i << " power = " << pow(PI, i) << "\n";
+	}
+}
+
+int main()
+{
+	double x, y, r;
+	cout << "Введите x: ";
+	cin >> x;
+	cout << "Введите y: ";
+	cin >> y;
+	perimeter_and_area(x, y);
+	add_substract_multiplicate_divide(x, y);
+	cout << "Введите радиус: ";
+	cin >> r;
+	circumference_and_area(r);
+	powers_of_pi();
+	return 0;
+}
